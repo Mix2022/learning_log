21feb8c08438930fe7ef810b3bd330d00a2805f8
Mikhail Kachargin <mix@post63.ru>
2023-02-17 12:23:41 +0400

    Редактирование данных: Добавление новых тем. Объект ModelForm. URL-адрес для new_topic. Функция представления new_topic(). Запросы GET и POST.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 class Topic(models.Model):
     """Тема, которую изучает пользователь"""
-    objects = None
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
