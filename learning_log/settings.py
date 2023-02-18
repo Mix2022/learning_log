c36fa9c3bf4bd7968ff98688df4890edab0523c6
Mikhail Kachargin <mix@post63.ru>
2023-02-19 00:32:02 +0400

    Создание учётных записей пользователей: Приложение users. Добавление пользователей в settings.py. Включение URL-адресов из users. Страница входа. Шаблон login. Создание ссылки на страницу входа. Добавление ссылки для выхода. Страница подтверждения выхода.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Приложения Django по умолчанию
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Мои приложения
     'learning_logs',
+    'users',
 ]
 
 MIDDLEWARE = [
